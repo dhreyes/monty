9a0ecdbe5b3e5fb652cb4b711552883b14ccf1aa
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-13 16:01:53 +0000

    implementing pop func

--- monty.h
+++ monty.h
@@ -50,6 +50,7 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 
 #endif
