620d80a7af5e67e626c361aff70db1b548a797a6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-13 08:24:34 +0000

    created second file for helpers

---
+++ helpers2_monty.c
@@ -0,0 +1,29 @@
+/**
+ *  *  * _atoi - turn string to int
+ *   *   * @s: string to be converted
+ *    *    * Return: Converted num
+ *     *     */
+int _atoi(char *s)
+{
+	int i = 0, len = 0, num = 0, sign = 1, ok = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len && ok == 0)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + sign * (s[i] - 48);
+			if (s[i + 1] >= '0' && s[i + 1] <= '9')
+				ok = 0;
+			else
+				ok = 1;
+		}
+		i++;
+	}
+	return (num);
+}
