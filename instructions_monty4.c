16946992a976e83e9e9a7001a7dcc01fa8d2215b
dhreyes <2590@holbertonschool.com>
2021-05-14 01:47:26 +0000

    Update instr for pstr

--- instructions_monty4.c
+++ instructions_monty4.c
@@ -23,3 +23,31 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 	printf("%c\n", number);
 }
+
+/**
+ * pstr - prints the str of the value of the 
+ * @stack: head of stack
+ * @line_number: line count
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	int number;
+	stack_t *str_candidate = *stack;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pstr, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (str_candidate != NULL)
+	{
+		number = str_candidate->n;
+		if (number >= 0 && number < 127)
+		{
+			printf("%c", number);
+			str_candidate = str_candidate->prev;
+		}
+	}
+	printf("\n");
+}
