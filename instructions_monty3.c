ee65b24cb2bae6994fb33741bbe63adbc0338b52
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-14 11:52:47 +0000

    fixed sawp edgecase and added stack length check

--- instructions_monty3.c
+++ instructions_monty3.c
@@ -10,28 +10,30 @@ void swap(stack_t **stack, unsigned int line_number)
 	stack_t *swap1 = *stack;
 	stack_t *swap2;
 
-	if (stack == NULL || (*stack)->next == NULL)
+	if (stackLength(*stack) < 2)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	swap2 = (*stack)->next;
-	if (swap2->next == NULL)
-	{
-		swap2->next = swap1;
-		swap2->prev = NULL;
-		swap1->next = NULL;
-		swap1->prev = swap2;
-		*stack = swap2;
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		node_num_value = "ERROR";
 	}
 	else
 	{
-		swap2->next->prev = swap1;
-		swap1->next = swap2->next;
-		swap1->prev = swap2;
-		swap2->next = swap1;
-		swap2->prev = NULL;
-		*stack = swap2;
+		swap2 = (*stack)->next;
+		if (swap2->next == NULL)
+		{
+			swap2->next = swap1;
+			swap2->prev = NULL;
+			swap1->next = NULL;
+			swap1->prev = swap2;
+			*stack = swap2;
+		}
+		else
+		{
+			swap2->next->prev = swap1;
+			swap1->next = swap2->next;
+			swap1->prev = swap2;
+			swap2->next = swap1;
+			swap2->prev = NULL;
+			*stack = swap2;
+		}
 	}
 }
