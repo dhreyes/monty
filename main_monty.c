31e24e5ac64a0aa3979df51e73b6b376405f45ed
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-16 22:36:14 +0000

    fixed no file passed case

--- main_monty.c
+++ main_monty.c
@@ -15,16 +15,18 @@ int main(int argc, char **argv)
 	char input[1000], **inputArr;
 	void (*func_ptr)(stack_t **, unsigned int) = NULL;
 	stack_t *stack = NULL;
+	FILE *fd;
 
-	FILE *fd = fopen(argv[1], "r");
-		if (fd == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(argv[1], "r");
+		if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
